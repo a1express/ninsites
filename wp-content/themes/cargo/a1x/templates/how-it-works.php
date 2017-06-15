f490dd9db2db7d7f5d6cc1c747d2bd5ed0b7b7ac
Andrei Serban <andrei.serban1987@gmail.com>
2017-06-15 23:14:21 +0300

    Wording changes

--- wp-content/themes/cargo/a1x/templates/how-it-works.php
+++ wp-content/themes/cargo/a1x/templates/how-it-works.php
@@ -3,7 +3,7 @@
         <figure>
             <img src="<?php echo get_template_directory_uri(); ?>/img/a1x/how-it-works/icon1.jpg" alt="Step 1" />
         </figure>
-        <h2>IT’s AS EASY AS A-B-C</h2>
+        <h2>SCHEDULING YOUR PICKUP IS AS EASY AS A-B-C</h2>
     </header>
     <div class="steps">
         <div class="step">
@@ -13,10 +13,12 @@
             <div class="content">
                 <div class="point">a</div>
                 <div class="text">
-                    <h3>ORDER BY PHONE, APP OR ONLINE</h3>
+                    <h3>Order By Phone, App or Online</h3>
                     <p>
                         You can schedule a pickup from wherever you are.<br/>
-                        Get price quote and delivery times with a few clicks.
+                        Get a price quote and delivery times with a few clicks.<br/>
+                        From envelopes to pallets, baked goods to large, big and <br/>
+                        bulky stuff, we'll pick up and deliver on time...every time.
                     </p>
                 </div>
             </div>
