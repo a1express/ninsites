3af19235ba65a87ccfd21950369124f7b494be3b
Andrei Serban <andrei.serban1987@gmail.com>
2017-06-20 23:30:48 +0300

    Body class

--- wp-content/themes/cargo/a1x/ext/body-class.php
+++ wp-content/themes/cargo/a1x/ext/body-class.php
@@ -2,6 +2,9 @@
 
 class BodyClass {
     public static function Init($classes) {
+
+        var_dump($classes); die();
+
         return $classes . ' ' . str_replace( array( 'http://', 'https://', '.com', 'www.' ), '', get_bloginfo('siteurl') ) . '-site';
     }
 }
