d85623e15a8b83c4de5397b7f5ea080c53059ede
Andrei Serban <andrei.serban1987@gmail.com>
2017-07-06 13:12:56 +0300

    How it works image

--- wp-content/themes/cargo/a1x/ext/how-it-works.php
+++ wp-content/themes/cargo/a1x/ext/how-it-works.php
@@ -8,13 +8,16 @@ class HowItWorks {
 
         $target1 = home_url('/wp-content/uploads/2015/09/howitworks3.png');
         $target2 = home_url('/wp-content/uploads/2015/09/howitworks4.png');
+        $target3 = home_url('/wp-content/uploads/2017/07/howitworks4.png');
 
         $target1 = sprintf( '<img src="%s" alt="%s">', $target1, $target1 );
         $target2 = sprintf( '<img src="%s" alt="%s">', $target2, $target2 );
+        $target3 = sprintf( '<img src="%s" alt="%s">', $target3, $target3 );
 
         return str_replace(array(
                 $target1,
-                $target2
+                $target2,
+                $target3
             ),
             $howItWorks,
             $content
