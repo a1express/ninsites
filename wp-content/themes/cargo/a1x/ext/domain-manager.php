5d8fd36d722a9915a63cd0760baf60f8e986bc5f
Mark McCurry <mark@a1express.com>
2017-07-12 14:08:05 -0400

    put everything back because it broke all sites

--- wp-content/themes/cargo/a1x/ext/domain-manager.php
+++ wp-content/themes/cargo/a1x/ext/domain-manager.php
@@ -5,7 +5,7 @@ class DomainManager {
     const DOMAIN_PROFICIENT_LOGISTIC = "proficientlogistic.com";
     const DOMAIN_EXPRESS_WAY_COURIER = "expresswaycourier.com";
     const DOMAIN_ASAP_COURIER = "asapcourierfl.com";
-    const DOMAIN_DEV_COURIER = "dev.boxonaplane.com";
+    
 
     const QQ_MANH_USERNAME = "remote";
     const QQ_MANH_PASSWORD = "remotequote";
@@ -27,10 +27,7 @@ class DomainManager {
     const QQ_ASAP_WEBSITE = "a1express";
     const QQ_ASAP_CUSTOMER = "A1XQOUTE";
 
-    const QQ_DEV_USERNAME = "remote";
-    const QQ_DEV_PASSWORD = "remotequote";
-    const QQ_DEV_WEBSITE = "a1express";
-    const QQ_DEV_CUSTOMER = "A1XQOUTE";
+    
 
     public static function IsManhattanCourierServiceDomain()
     {
@@ -60,12 +57,7 @@ class DomainManager {
         return $domain == self::DOMAIN_ASAP_COURIER || strpos( $domain, self::DOMAIN_ASAP_COURIER ) !== false;
     }
 
-    public static function IsDEVCourierDomain()
-    {
-        $domain = self::GetCurrentDomain();
-
-        return $domain == self::DOMAIN_DEV_COURIER || strpos( $domain, self::DOMAIN_DEV_COURIER ) !== false;
-    }
+   
 
     public static function IsLocalhostDomain()
     {
@@ -80,7 +72,7 @@ class DomainManager {
         return isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : '';
     }
 
-    public static function GetVariable($manhattanVariable, $proficientVariable, $expressVariable, $asapVariable, $devVariable, $localhostVariable, $defaultVariable = '')
+    public static function GetVariable($manhattanVariable, $proficientVariable, $expressVariable, $asapVariable, $localhostVariable, $defaultVariable = '')
     {
         $variable = $defaultVariable;
 
@@ -92,8 +84,6 @@ class DomainManager {
             $variable = $expressVariable;
         else if ( self::IsASAPCourierDomain() )
             $variable = $asapVariable;
-        else if ( self::IsDEVCourierDomain() )
-            $variable = $devVariable;
         else if ( self::IsLocalhostDomain() )
             $variable = $localhostVariable;
 
--- wp-content/themes/cargo/a1x/ext/solve-360.php
+++ wp-content/themes/cargo/a1x/ext/solve-360.php
@@ -12,6 +12,8 @@ class Solve360 {
                 $url = 'http://www.webicise.com/Solve360/Expressway/Calculator/Solve360ContactSave.php';
 	    else if (DomainManager::IsASAPCourierDomain())
                 $url = 'http://www.webicise.com/Solve360/ASAP/Calculator/Solve360ContactSave.php';
+	    else if (DomainManager::IsDEVCourierDomain())
+                $url = 'http://www.webicise.com/Solve360/DEV/Calculator/Solve360ContactSave.php';
             $data = array(
                 'company' => '',
                 'firstname' => '',
