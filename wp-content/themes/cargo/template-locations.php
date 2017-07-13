22c851abae94d8ab5c315436ce34efa23fc79d12
Andrei Serban <andrei.serban1987@gmail.com>
2017-07-13 14:51:13 +0300

    Locations page

--- wp-content/themes/cargo/template-locations.php
+++ wp-content/themes/cargo/template-locations.php
@@ -5,50 +5,273 @@
 	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/a1x/js/jquery.vmap.min.js"></script>
 	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/a1x/js/jquery.vmap.usa.js"></script>
 
-	<section class="boldSection bottomSpaced gutter inherit">
-		<div class="port">
-			<div class="boldCell">
-				<div class="boldCellInner">
-					<div class="boldRow">
-						<div class="rowItem col-md-12">
-							<div class="rowItemContent">
-								<h3>Warehouse Locations</h3>
+	<div class="locations-wrapper">
+		<section class="boldSection topSpaced bottomSpaced gutter inherit">
+			<div class="port">
+				<div class="boldCell">
+					<div class="boldCellInner">
+						<div class="boldRow">
+							<div class="rowItem col-md-12">
+								<div class="rowItemContent">
+									<h3>Warehouse Locations</h3>
+								</div>
 							</div>
-						</div>
-						<div class="rowItem col-md-6 inherit">
-							<div class="rowItemContent">
-								<img src="<?php echo get_template_directory_uri(); ?>/img/a1x/warehouse_map.png" alt="Warehouses" />
+							<div class="rowItem col-md-6 inherit">
+								<div class="rowItemContent map-bullets">
+									<img src="<?php echo get_template_directory_uri(); ?>/img/a1x/warehouse_map1.png" alt="Warehouses" />
+
+									<div class="bullet ny">
+										<strong>NY</strong>
+										NY/METRO AREA
+									</div>
+									<div class="bullet ky">
+										<strong>KY</strong>
+										KENTUCKY
+									</div>
+									<div class="bullet md">
+										<strong>MD</strong>
+										BALTIMORE
+									</div>
+									<div class="bullet nc">
+										<strong>NC</strong>
+										CHARLOTTE
+									</div>
+									<div class="bullet sc">
+										<strong>SC</strong>
+										CHARLESTON
+									</div>
+									<div class="bullet ga">
+										<strong>GA</strong>
+										ATLANTA
+									</div>
+									<div class="bullet fl1">
+										<strong>FL</strong>
+										ORLANDO
+									</div>
+									<div class="bullet fl2">
+										<strong>FL</strong>
+										FT. LAUDERDALE
+									</div>
+								</div>
 							</div>
-						</div>
-						<div class="rowItem col-md-6 inherit">
-							<div class="rowItemContent">
-								<header class="header btClear large customHeaderContent btLightAccentDash btRegularTitle">
-									<br/><br/><br/>
-									<h3>Learn more about our<br/>Warehouse Locations</h3>
-									<br/>
-									<img src="//nindelivers.com/wp-content/uploads/2017/02/warehouse.jpg" alt="Warehouse" />
-								</header>
 
-								<div class="info-box" style="display: none;">
-									<div class="title">NY/Metro Area Warehouse</div>
-									<div class="body">
-										<div class="two-cols">
-											<div class="column">
+							<div class="rowItem col-md-6 inherit">
+								<div class="rowItemContent">
+									<header class="header btClear large customHeaderContent btLightAccentDash btRegularTitle default-box">
+										<br/><br/><br/>
+										<h3>Learn more about our<br/>Warehouse Locations</h3>
+										<br/>
+										<img src="//nindelivers.com/wp-content/uploads/2017/02/warehouse.jpg" alt="Warehouse" />
+									</header>
 
+									<div class="info-box ny">
+										<h3 class="title">NY/Metro Area Warehouse</h3>
+										<div class="body">
+											<div class="two-cols">
+												<div class="column">
+													<img src="//nindelivers.com/wp-content/uploads/2017/02/warehouse.jpg" alt="Warehouse" />
+												</div>
+												<div class="column">
+													NY/Metro Area 45,000 sq.ft.<br/>
+													29 Spring St.<br/>
+													West Orange, NJ 07052<br/>
+													973-621-6010
+												</div>
 											</div>
-											<div class="column">
-												<img src="//nindelivers.com/wp-content/uploads/2017/02/warehouse.jpg" alt="Warehouse" />
+											<div class="three-cols">
+												<div class="column">
+													Mafalda DaCosta<br/>
+													Director of FTL, LTL, TSA<br/>
+													ext.351<br/>
+													mdacosta@sdsgl.com
+												</div>
+												<div class="column">
+													Mafalda DaCosta<br/>
+													Director of FTL, LTL, TSA<br/>
+													ext.351<br/>
+													mdacosta@sdsgl.com
+												</div>
+												<div class="column">
+													Mafalda DaCosta<br/>
+													Director of FTL, LTL, TSA<br/>
+													ext.351<br/>
+													mdacosta@sdsgl.com
+												</div>
 											</div>
 										</div>
-										<div class="three-cols">
-											<div class="column">
-												<img src="//nindelivers.com/wp-content/uploads/2017/02/warehouse.jpg" alt="Warehouse" />
-											</div>
-											<div class="column">
+									</div>
 
+									<div class="info-box ky">
+										<h3 class="title">Kentucky</h3>
+										<div class="body">
+											<div class="two-cols">
+												<div class="column">
+													<img src="//nindelivers.com/wp-content/uploads/2017/02/warehouse.jpg" alt="Warehouse" />
+												</div>
+												<div class="column">
+													NY/Metro Area 45,000 sq.ft.
+												</div>
 											</div>
-											<div class="column">
+											<div class="three-cols">
+												<div class="column">
+													Mafalda DaCosta
+												</div>
+												<div class="column">
+													Mafalda DaCosta
+												</div>
+												<div class="column">
+													Mafalda DaCosta
+												</div>
+											</div>
+										</div>
+									</div>
 
+									<div class="info-box md">
+										<h3 class="title">Baltimore</h3>
+										<div class="body">
+											<div class="two-cols">
+												<div class="column">
+													<img src="//nindelivers.com/wp-content/uploads/2017/02/warehouse.jpg" alt="Warehouse" />
+												</div>
+												<div class="column">
+													NY/Metro Area 45,000 sq.ft.
+												</div>
+											</div>
+											<div class="three-cols">
+												<div class="column">
+													Mafalda DaCosta
+												</div>
+												<div class="column">
+													Mafalda DaCosta
+												</div>
+												<div class="column">
+													Mafalda DaCosta
+												</div>
+											</div>
+										</div>
+									</div>
+
+									<div class="info-box nc">
+										<h3 class="title">Charlotte</h3>
+										<div class="body">
+											<div class="two-cols">
+												<div class="column">
+													<img src="//nindelivers.com/wp-content/uploads/2017/02/warehouse.jpg" alt="Warehouse" />
+												</div>
+												<div class="column">
+													NY/Metro Area 45,000 sq.ft.
+												</div>
+											</div>
+											<div class="three-cols">
+												<div class="column">
+													Mafalda DaCosta
+												</div>
+												<div class="column">
+													Mafalda DaCosta
+												</div>
+												<div class="column">
+													Mafalda DaCosta
+												</div>
+											</div>
+										</div>
+									</div>
+
+									<div class="info-box sc">
+										<h3 class="title">Charleston</h3>
+										<div class="body">
+											<div class="two-cols">
+												<div class="column">
+													<img src="//nindelivers.com/wp-content/uploads/2017/02/warehouse.jpg" alt="Warehouse" />
+												</div>
+												<div class="column">
+													NY/Metro Area 45,000 sq.ft.
+												</div>
+											</div>
+											<div class="three-cols">
+												<div class="column">
+													Mafalda DaCosta
+												</div>
+												<div class="column">
+													Mafalda DaCosta
+												</div>
+												<div class="column">
+													Mafalda DaCosta
+												</div>
+											</div>
+										</div>
+									</div>
+
+									<div class="info-box ga">
+										<h3 class="title">Atlanta</h3>
+										<div class="body">
+											<div class="two-cols">
+												<div class="column">
+													<img src="//nindelivers.com/wp-content/uploads/2017/02/warehouse.jpg" alt="Warehouse" />
+												</div>
+												<div class="column">
+													NY/Metro Area 45,000 sq.ft.
+												</div>
+											</div>
+											<div class="three-cols">
+												<div class="column">
+													Mafalda DaCosta
+												</div>
+												<div class="column">
+													Mafalda DaCosta
+												</div>
+												<div class="column">
+													Mafalda DaCosta
+												</div>
+											</div>
+										</div>
+									</div>
+
+									<div class="info-box fl1">
+										<h3 class="title">Orlando</h3>
+										<div class="body">
+											<div class="two-cols">
+												<div class="column">
+													<img src="//nindelivers.com/wp-content/uploads/2017/02/warehouse.jpg" alt="Warehouse" />
+												</div>
+												<div class="column">
+													NY/Metro Area 45,000 sq.ft.
+												</div>
+											</div>
+											<div class="three-cols">
+												<div class="column">
+													Mafalda DaCosta
+												</div>
+												<div class="column">
+													Mafalda DaCosta
+												</div>
+												<div class="column">
+													Mafalda DaCosta
+												</div>
+											</div>
+										</div>
+									</div>
+
+									<div class="info-box fl2">
+										<h3 class="title">Ft. Lauderdale</h3>
+										<div class="body">
+											<div class="two-cols">
+												<div class="column">
+													<img src="//nindelivers.com/wp-content/uploads/2017/02/warehouse.jpg" alt="Warehouse" />
+												</div>
+												<div class="column">
+													NY/Metro Area 45,000 sq.ft.
+												</div>
+											</div>
+											<div class="three-cols">
+												<div class="column">
+													Mafalda DaCosta
+												</div>
+												<div class="column">
+													Mafalda DaCosta
+												</div>
+												<div class="column">
+													Mafalda DaCosta
+												</div>
 											</div>
 										</div>
 									</div>
@@ -58,82 +281,126 @@
 					</div>
 				</div>
 			</div>
-		</div>
-	</section>
+		</section>
 
-	<section class="boldSection bottomSpaced gutter inherit">
-		<div class="port">
-			<div class="boldCell">
-				<div class="boldCellInner">
-					<div class="boldRow">
-						<div class="rowItem col-md-12 inherit">
-							<div class="rowItemContent">
-								<script type="text/javascript">
-									jQuery(document).ready(function(){
-										jQuery('#vmap').vectorMap({
-											map: 'usa_en',
-											backgroundColor: null,
-											color: '#0a1a77',
-											enableZoom: false,
-											showTooltip: true,
-											selectedColor: null,
-											hoverColor: null,
-											colors: {
-												ak: '#f5f5f5',
-												hi: '#f5f5f5',
-												ny: '#27521f',
-												pa: '#27521f',
-												nj: '#27521f',
-												ct: '#0b7a46',
-												il: '#94765f',
-												ky: '#f78b8b',
-												nc: '#884c9e',
-												sc: '#517594',
-												va: '#a82828',
-												md: '#a82828',
-												ga: '#d1d100',
-												fl: '#cf1919'
-											},
-											onRegionClick: function(event, code, region){
-												event.preventDefault();
-											},
-											onLabelShow: function(event, label, code) {
-												if (code == 'ak' || code == 'hi') {
-													event.preventDefault();
-												} else if ( code == 'il' ) {
-													label[0].innerHTML = '<strong>Chicago Office</strong>';
-												} else if ( code == 'nc' ) {
-													label[0].innerHTML = '<strong>North Carolina Warehouse</strong>';
-												} else if ( code == 'ny' || code == 'pa' ) {
-													label[0].innerHTML = '<strong>Connecticut, Southern New England Office,</strong><br/>expresswaycourier.com 203-790-1755';
-												} else if ( code == 'ct' ) {
-													label[0].innerHTML = '<strong>Connecticut, Southern New England Office,</strong><br/>expresswaycourier.com 203-790-1755';
-												} else if ( code == 'nc' ) {
-													label[0].innerHTML = '<strong>Connecticut, Southern New England Office,</strong><br/>expresswaycourier.com 203-790-1755';
-												} else if ( code == 'sc' ) {
-													label[0].innerHTML = '<strong>Connecticut, Southern New England Office,</strong><br/>expresswaycourier.com 203-790-1755';
-												} else if ( code == 'ga' ) {
-													label[0].innerHTML = '<strong>Connecticut, Southern New England Office,</strong><br/>expresswaycourier.com 203-790-1755';
-												} else if ( code == 'fl' ) {
-													label[0].innerHTML = '<strong>Connecticut, Southern New England Office,</strong><br/>expresswaycourier.com 203-790-1755';
-												} else {
-													label[0].innerHTML = '<strong>' + label[0].innerHTML + '</strong><br/>https://nindelivers.com +1 (800) 469-0929';
-												}
-											}
-										});
-									});
-								</script>
+		<section class="boldSection bottomSpaced gutter inherit">
+			<div class="port">
+				<div class="boldCell">
+					<div class="boldCellInner">
+						<div class="boldRow">
+							<div class="rowItem col-md-12 inherit">
+								<div class="rowItemContent">
+									<script type="text/javascript">
+										(function($) {
+											$(document).ready(function(){
+												$(".bullet").click(function(){
+													var targetClass = $(this).attr('class').replace("bullet ", "");
 
-								<h3>Office Locations</h3>
-								<h4>(Click location for more info)</h4>
-								<div id="vmap" style="width: 100%; height: 600px;"></div>
+													$(".default-box").hide();
+													$(".info-box").removeClass("active").filter("." + targetClass).addClass('active');
+												});
+
+												$(".map-info-box .close").on("click", function(){
+													$(".map-info-box").removeClass("active");
+												});
+
+												$('#vmap').vectorMap({
+													map: 'usa_en',
+													backgroundColor: null,
+													color: '#0a1a77',
+													enableZoom: false,
+													showTooltip: true,
+													selectedColor: null,
+													hoverColor: null,
+													colors: {
+														ak: '#f5f5f5',
+														hi: '#f5f5f5',
+														ny: '#27521f',
+														pa: '#27521f',
+														nj: '#27521f',
+														ct: '#0b7a46',
+														il: '#94765f',
+														ky: '#f78b8b',
+														nc: '#884c9e',
+														sc: '#517594',
+														va: '#a82828',
+														md: '#a82828',
+														ga: '#d1d100',
+														fl: '#cf1919'
+													},
+													onRegionClick: function(event, code, region){
+														event.preventDefault();
+
+														$(".map-info-box").removeClass("active");
+
+														var target = $(".map-info-box." + code);
+														if ( target.length > 0 )
+															target.addClass('active');
+														else
+															$(".map-info-box.map-default-box").addClass("active");
+													},
+													onLabelShow: function(event, label, code) {
+														if (code == 'ak' || code == 'hi') {
+															event.preventDefault();
+														} else if ( code == 'il' ) {
+															label[0].innerHTML = '<strong>Chicago Office</strong>';
+														} else if ( code == 'nc' ) {
+															label[0].innerHTML = '<strong>North Carolina Warehouse</strong>';
+														} else if ( code == 'ny' || code == 'pa' ) {
+															label[0].innerHTML = '<strong>Connecticut, Southern New England Office,</strong><br/>expresswaycourier.com 203-790-1755';
+														} else if ( code == 'ct' ) {
+															label[0].innerHTML = '<strong>Connecticut, Southern New England Office,</strong><br/>expresswaycourier.com 203-790-1755';
+														} else if ( code == 'nc' ) {
+															label[0].innerHTML = '<strong>Connecticut, Southern New England Office,</strong><br/>expresswaycourier.com 203-790-1755';
+														} else if ( code == 'sc' ) {
+															label[0].innerHTML = '<strong>Connecticut, Southern New England Office,</strong><br/>expresswaycourier.com 203-790-1755';
+														} else if ( code == 'ga' ) {
+															label[0].innerHTML = '<strong>Connecticut, Southern New England Office,</strong><br/>expresswaycourier.com 203-790-1755';
+														} else if ( code == 'fl' ) {
+															label[0].innerHTML = '<strong>Connecticut, Southern New England Office,</strong><br/>expresswaycourier.com 203-790-1755';
+														} else {
+															label[0].innerHTML = '<strong>' + label[0].innerHTML + '</strong><br/>https://nindelivers.com +1 (800) 469-0929';
+														}
+													}
+												});
+											});
+										})( jQuery );
+									</script>
+
+									<h3>Office Locations</h3>
+									<h4>(Click location for more info)</h4>
+									<div id="vmap" style="width: 100%; height: 600px;"></div>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
+		</section>
+
+		<div class="map-info-box ny">
+			<div class="map-info-box-inside">
+				<div class="close">
+					<span>Close</span>
+				</div>
+
+				<h4>Title here</h4>
+				<p>Info here</p>
+			</div>
 		</div>
-	</section>
+
+		<div class="map-info-box map-default-box">
+			<div class="map-info-box-inside">
+				<div class="close">
+					<span>Close</span>
+				</div>
+
+				<h4>Default title here</h4>
+				<p>Default info here</p>
+			</div>
+		</div>
+
+	</div>
 
 <?php
 
