98369d8be08587e4f5148bcaec53f387fbaacde4
Mark McCurry <mark@a1express.com>
2017-08-17 07:54:22 -0400

    QQ Changes

--- wp-content/themes/cargo/a1x/ext/quick-quote.php
+++ wp-content/themes/cargo/a1x/ext/quick-quote.php
@@ -8,14 +8,16 @@ class QuickQuote {
                 ( ( DomainManager::IsManhattanCourierServiceDomain() || DomainManager::IsLocalhostDomain() ) && ( $pageId == 76 || $pageId == 1765 ) )
                 || ( DomainManager::IsProficientLogisticDomain() && ( $pageId == 2399 || $pageId == 2344 ) )
                 || ( DomainManager::IsExpressWayCourierDomain() && ( $pageId == 1682 || $pageId == 1711 ) )
-		|| ( DomainManager::IsASAPCourierDomain() && ( $pageId == 1683 || $pageId == 1711 ) )
-		|| ( DomainManager::IsDEVCourierDomain() && ( $pageId == 1683 || $pageId == 1711 ) )
+		        || ( DomainManager::IsASAPCourierDomain() && ( $pageId == 1683 || $pageId == 1711 || $pageId == 1936 ) )
+		        || ( DomainManager::IsDEVCourierDomain() && ( $pageId == 1683 || $pageId == 1711 ) )
                 || ( DomainManager::IsMMCourierDomain() && ( $pageId == 1684 || $pageId == 1698 ) )
             )
         {
             ob_start();
             get_template_part( "a1x/templates/quick-quote" );
             $quickquote = ob_get_clean();
+
+            $quickquote = str_replace( '<div id="qq-holder" class="gutter">', '<div id="qq-holder" class="in-page gutter">', $quickquote );
 
             $target = '<div class="slided mediumSliderHeight';
 
