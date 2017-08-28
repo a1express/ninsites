025dc36696881b6290a33480445184349734ab97
Mark McCurry <mark@a1express.com>
2017-08-28 10:42:35 -0400

    added QQ pages to nyc site

--- wp-content/themes/cargo/a1x/ext/quick-quote.php
+++ wp-content/themes/cargo/a1x/ext/quick-quote.php
@@ -13,7 +13,7 @@ class QuickQuote
 		        || ( DomainManager::IsASAPCourierDomain() && ( $pageId == 1683 || $pageId == 1711 || $pageId == 1936 || $pageId == 1940) )
 		        || ( DomainManager::IsDEVCourierDomain() && ( $pageId == 1683 || $pageId == 1711 ) )
                 || ( DomainManager::IsMMCourierDomain() && ( $pageId == 1684 || $pageId == 1698 ) )
-                || ( DomainManager::IsNYCourierDomain() && ( $pageId == 1684 || $pageId == 1698 ) )
+                || ( DomainManager::IsNYCourierDomain() && ( $pageId == 1753 || $pageId == 1765 ) )
             )
         {
             ob_start();
