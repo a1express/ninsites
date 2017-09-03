45371e214cc7e858c30ec8148dd5178f2f47219c
Mark McCurry <mark@a1express.com>
2017-09-03 08:08:07 -0400

    added SOS, SDGL & NIN to QQ ext page

--- wp-content/themes/cargo/a1x/ext/quick-quote.php
+++ wp-content/themes/cargo/a1x/ext/quick-quote.php
@@ -14,6 +14,9 @@ class QuickQuote
 		        || ( DomainManager::IsDEVCourierDomain() && ( $pageId == 1683 || $pageId == 1711 ) )
                 || ( DomainManager::IsMMCourierDomain() && ( $pageId == 1684 || $pageId == 1698 ) )
                 || ( DomainManager::IsNYCourierDomain() && ( $pageId == 1753 || $pageId == 1765 ) )
+		|| ( DomainManager::IsSdSglCourierDomain() && ( $pageId == 1753 || $pageId == 1765 ) )
+		|| ( DomainManager::IsSosCourierDomain() && ( $pageId == 1610 || $pageId == 1667 ) )
+		|| ( DomainManager::IsNinCourierDomain() && ( $pageId == 1683 || $pageId == 1711 ) )
             )
         {
             ob_start();
