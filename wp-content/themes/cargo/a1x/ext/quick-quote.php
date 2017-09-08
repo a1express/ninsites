0896abc442905d8f88092f1fb270692743ba71e8
Andrei Serban <andrei.serban1987@gmail.com>
2017-09-08 16:18:09 +0300

    3. I launched nindelivers.com over the weekend and no matter what I did, it messed up all sites.

--- wp-content/themes/cargo/a1x/ext/quick-quote.php
+++ wp-content/themes/cargo/a1x/ext/quick-quote.php
@@ -14,6 +14,7 @@ class QuickQuote
 		|| ( DomainManager::IsDEVCourierDomain() && ( $pageId == 1683 || $pageId == 1711 ) )
                 || ( DomainManager::IsMMCourierDomain() && ( $pageId == 1684 || $pageId == 1698 ) )
                 || ( DomainManager::IsNYCourierDomain() && ( $pageId == 1753 || $pageId == 1765 ) )
+		|| ( DomainManager::IsNinCourierDomain() && ( $pageId == 1683 || $pageId == 1711 ) )
             )
         {
             ob_start();
